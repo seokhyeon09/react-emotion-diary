5463768faaa484ca18c0f990aca38141af95caad
seokhyeon09 <ring4392@gmail.com>
2026-02-23 11:03:50 +0900

    new, editor, emotionitem update

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Editor from '../components/Editor'
 
 const Edit = () => {
   const params = useParams()
 
   return (
-    <div>{params.id}번 Edit</div>
+    <div>
+      <Header
+        leftChild={<Button text={'뒤로가기'}/>}
+        title={'일기 수정하기'}
+        rightChild={<Button text={'삭제하기'} type={'NEGATIVE'}/>}
+      />
+      <Editor/>
+    </div>
   )
 }
 
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Editor from '../components/Editor'
+import Button from '../components/Button'
+import { DiaryDispatchContext } from '../App'
 
 const New = () => {
+  const nav = useNavigate()
+  const { onCreate } = useContext(DiaryDispatchContext)
+
+  const onSumit = (input) => {
+    onCreate(
+      input.createdDate.getTime(),
+      input.emotionId,
+      input.content,
+    )
+    nav('/',{replace:true})
+  }
   return (
-    <div>New</div>
+    <div>
+      <Header
+        title={'새 일기 쓰기'}
+        leftChild={
+          <Button
+            text={'< 뒤로 가기'}
+            onClick={() => nav(-1)}
+          />}
+      />
+      <Editor onSumit={onSumit} />
+
+    </div>
   )
 }
 
